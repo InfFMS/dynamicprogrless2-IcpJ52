47fe1a0d5205636199f2a1d794c51b5214390d71
IcpJ42 <iblion08@gmail.com>
2025-12-11 11:12:20 +0700

    Сделал 1 задание

--- task1.py
+++ task1.py
@@ -20,3 +20,15 @@
 Ответ должен быть напечатан как одно число.
 """
 
+
+def f(a, b):
+    if a == 10:
+        return 0
+    if a == b:
+        return 1
+    if a > b:
+        return 0
+    return f(a + 1, b) + f(a + 2, b) + f(a * 2, b)
+
+
+print(f(3, 7) * f(7, 20))
