2370e8be8fdd882d6ac3a44d68eff81ad61a1218
IcpJ42 <iblion08@gmail.com>
2025-12-13 09:31:39 +0700

    Сделал 10 задание

--- task10.py
+++ task10.py
@@ -12,7 +12,17 @@ C: Увеличить количество снежинок в 2 раза (num =
 
 Ответ должен быть напечатан как одно число.
 """
+import sys
+
+sys.setrecursionlimit(10000)
 
 
+def f(a, b):
+    if a == b:
+        return 1
+    if a > b:
+        return 0
+    return f(a + 1, b) + f(a + 2, b) + f(a * 2, b)
 
 
+print(f(5, 13) * f(13, 25))
